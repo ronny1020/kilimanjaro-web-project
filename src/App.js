ff1dcd167c4b1d882ab9e95d43eeec25e056b53d
Ronny Hsieh <1020ronny@gmail.com>
2020-04-17 12:05:44 +0800

    purchase router

--- src/App.js
+++ src/App.js
@@ -6,18 +6,30 @@ import {
   Redirect,
 } from 'react-router-dom'
 
+// import pages on navbar
 import Home from './pages/Home'
 import ProductList from './pages/ProductList'
 import Sellers from './pages/Sellers'
 import OnSale from './pages/OnSale'
 import About from './pages/About'
 
+// others
 import Product from './pages/Product'
 
+// import member pages
 import Login from './pages/member/Login'
 import Lobby from './pages/member/logged/Lobby'
+
+// import purchase pages
+import Cart from './pages/Cart'
+import Shipment from './pages/purchase/Shipment'
+import Payment from './pages/purchase/Payment'
+import PurchaseComplied from './pages/purchase/PurchaseComplied'
+
+// import not found
 import NotFoundPage from './pages/NotFoundPage'
 
+// import grid part
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -29,6 +41,7 @@ function App() {
           <Header />
 
           <Switch>
+            {/* navbar pages */}
             <Route exact path="/">
               <Home />
             </Route>
@@ -45,10 +58,12 @@ function App() {
               <About />
             </Route>
 
+            {/* others */}
             <Route path="/product/:id?">
               <Product />
             </Route>
 
+            {/* member pages */}
             <Route path="/login">
               <Redirect from="/login" to="/login/entrance"></Redirect>
               <Login />
@@ -57,6 +72,21 @@ function App() {
               <Lobby />
             </Route>
 
+            {/* purchase pages */}
+            <Route path="/cart">
+              <Cart />
+            </Route>
+            <Route path="/shipment">
+              <Shipment />
+            </Route>
+            <Route path="/payment">
+              <Payment />
+            </Route>
+            <Route path="/purchaseComplied">
+              <PurchaseComplied />
+            </Route>
+
+            {/* not found */}
             <Route path="*">
               <NotFoundPage />
             </Route>
