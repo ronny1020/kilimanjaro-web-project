58a41d503a3dfc5eb6b64f6e561cd9e3f6f793c0
YU LI <ck9030311@gmail.com>
2020-04-17 11:50:27 +0800

    0417

---
+++ src/pages/member/logged/Coupon.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import LobbyTitle from '../../../components/member/LobbyTitle'
+import Sidebar from '../../../components/Sidebar'
+
+function Coupon() {
+  const inputArray = {
+    title: '會員中心',
+    個人資料修改: {
+      link: '/lobby/edit',
+    },
+
+    折扣券: {
+      link: '/lobby/coupon',
+    },
+    交易紀錄: {
+      link: '/lobby/history',
+    },
+    喜好清單: {
+      link: '/lobby/favorite',
+    },
+  }
+  return (
+    <>
+      <LobbyTitle string={'折扣券'} />
+      <Sidebar input={inputArray} />
+    </>
+  )
+}
+
+export default Coupon
