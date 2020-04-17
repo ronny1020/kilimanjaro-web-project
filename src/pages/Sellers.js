45ae6e7a15e06d3286aa4bf480dcc9499f0c0007
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-04-17 16:10:54 +0800

    4/17

--- src/pages/Sellers.js
+++ src/pages/Sellers.js
@@ -3,7 +3,15 @@ import React from 'react'
 function Sellers() {
   return (
     <>
-      <h1>賣家介紹</h1>
+      <div class="container p-3 my-3 bg-primary text-white">
+        <h1>賣家介紹</h1>
+        <p>This part is inside a .container class.</p>
+        <p>The .container class provides a responsive fixed width container.</p>
+        <p>
+          Resize the browser window to see that its width (max-width) will
+          change at different breakpoints.
+        </p>
+      </div>
     </>
   )
 }
