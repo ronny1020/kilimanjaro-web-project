c4348c7c285b0e40c921b1d00c945eb8739f2111
Ronny Hsieh <1020ronny@gmail.com>
2020-04-20 18:31:16 +0800

    fetch productList

--- src/actions/getProductList.js
+++ src/actions/getProductList.js
@@ -1,33 +1,12 @@
 import { GET_PRODUCT_LIST } from './actionTypes'
 
-// export const getProductList = () => (dispatch) => {
-//   fetch('http://localhost:6001/ProductListApi')
-//     .then((res) => res.json())
-//     .then((lists) => {
-//       console.log(lists)
-//       return dispatch({
-//         type: GET_PRODUCT_LIST,
-//         payload: lists,
-//       })
-//     })
-// }
-
 export const getProductList = (payload) => {
-  return async function getTotalFromServer(dispatch) {
-    const request = new Request('http://localhost:6001/ProductListApi', {
-      method: 'GET',
-      headers: new Headers({
-        Accept: 'application/json',
-        'Content-Type': 'appliaction/json',
-      }),
-    })
-
-    const response = await fetch(request)
-    const lists = await response.json()
-
+  return async function getProductListFetch(dispatch) {
+    const response = await fetch('http://localhost:6001/ProductListApi')
+    const list = await response.json()
     dispatch({
       type: GET_PRODUCT_LIST,
-      payload: lists,
+      payload: list,
     })
   }
 }
