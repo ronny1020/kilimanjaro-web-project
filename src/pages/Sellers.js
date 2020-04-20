d3a222b63e6d4ae4dac0bdb890323a615276b949
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-04-20 10:30:08 +0800

    0420

--- src/pages/Sellers.js
+++ src/pages/Sellers.js
@@ -1,18 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Sellers() {
   return (
     <>
-      <div class="container">
-        <h1>賣家介紹</h1>
+      <div class="row justify-content-center align-items-center">
+        <div class="col-md-8 mt-3 ">
+          <h3>
+            賣家介紹
+            <Link to="/product/123">123</Link>
+          </h3>
+        </div>
       </div>
-      <div class="container p-3 my-3 border">
-        <p>This part is inside a .container class.</p>
-        <p>The .container class provides a responsive fixed width container.</p>
-        <p>
-          Resize the browser window to see that its width (max-width) will
-          change at different breakpoints.
-        </p>
+
+      <div class="row justify-content-center align-items-center">
+        <div class="col-md-8 ">
+          <div class="container p-3 my-3 border ">
+            <p>
+              Resize the browser window to see that its width (max-width) will
+              change at different breakpoints.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   )
