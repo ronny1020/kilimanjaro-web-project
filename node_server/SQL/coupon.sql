74778a5c16f045da26d46eb23bdbbfc2eabd4422
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-04-23 15:47:14 +0800

    新增庫碰資料

---
+++ node_server/SQL/coupon.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS coffee.coupon;
+CREATE TABLE coffee.coupon (
+  couponID int AUTO_INCREMENT PRIMARY KEY,-- Primary key:庫碰編號
+  couponName varchar(20) collate utf8_unicode_ci NOT NULL,-- 庫碰名稱
+  productID int(10) ZEROFILL NOT NULL,-- 對應商品
+  minus int(2) ZEROFILL NOT NULL,-- 扣除額
+  cpendDate date default NULL, -- 過期時間
+  CONSTRAINT pair UNIQUE (productID)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+CREATE INDEX productID ON coffee.coupon (productID);
+
+INSERT INTO coffee.coupon (couponName,productID,minus,cpendDate) VALUES ('20元折抵',0000164413,20,'2020-06-30'),('20元折抵',0000164415,20,'2020-06-30'),('20元折抵',0000164513,20,'2020-06-30');
