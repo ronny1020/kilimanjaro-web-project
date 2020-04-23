13d03f09e2641a32fed12da43ace950e45b03ad6
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-04-23 16:13:53 +0800

    新增持有人

--- node_server/SQL/coupon.sql
+++ node_server/SQL/coupon.sql
@@ -3,7 +3,7 @@ CREATE TABLE coffee.coupon (
   couponID int AUTO_INCREMENT PRIMARY KEY,-- Primary key:庫碰編號
   couponName varchar(20) collate utf8_unicode_ci NOT NULL,-- 庫碰名稱
   productID int(10) ZEROFILL NOT NULL,-- 對應商品
-  customerID varchar(5) NOT NULL,--持有人
+  customerID varchar(5) NOT NULL,-- 持有人
   minus int(2) ZEROFILL NOT NULL,-- 扣除額
   cpendDate date default NULL, -- 過期時間
   CONSTRAINT pair UNIQUE (productID,customerID)
