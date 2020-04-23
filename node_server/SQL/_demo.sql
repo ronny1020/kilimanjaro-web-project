0323e58ea4f814f76e5d6a2010e2b33893a7ad81
Ronny Hsieh <1020ronny@gmail.com>
2020-04-23 17:12:17 +0800

    coupon sql change

--- node_server/SQL/_demo.sql
+++ node_server/SQL/_demo.sql
@@ -13490,6 +13490,42 @@ INSERT INTO coffee.comments  (productID , customerID ,rate, commentText) VALUES
 (0000164415,'C001', 4 ,"good"),
 (0000164513,'C001', 3 ,"It's OK...");
 
+
+
+
+
+
+DROP TABLE IF EXISTS coffee.coupon;
+CREATE TABLE coffee.coupon (
+  couponID int AUTO_INCREMENT PRIMARY KEY,-- Primary key:庫碰編號
+  couponName varchar(20) collate utf8_unicode_ci NOT NULL,-- 庫碰名稱
+  minus int NOT NULL, -- 扣除額
+  limitation int, -- 折扣門檻
+  cpendDate date default NULL, -- 過期時間
+  used boolean
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+INSERT INTO coffee.coupon (couponName,minus,limitation,cpendDate)
+ VALUES ('20元折抵',20,200,'2020-06-30'),
+		('30元折抵',30,300,'2020-06-30'),
+		('40元折抵',40,400,'2020-06-30'),
+		('100元折抵',100,1000,'2020-06-30');
+
+
+
+DROP TABLE IF EXISTS coffee.couponMap;
+CREATE TABLE coffee.couponMap (
+  couponMapId  int AUTO_INCREMENT PRIMARY KEY,-- Primary key:編號
+  couponID int ,-- 庫碰編號
+  customerID varchar(5) NOT NULL,-- 持有人
+  valid boolean,
+  CONSTRAINT pair UNIQUE (couponID,customerID)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+INSERT INTO coffee.couponMap (couponID,customerID,valid)
+ VALUES (1,'C001',true),(2,'C001',true),(1,'C002',true),(3,'C001',true),(3,'C002',true),(1,'C005',true),(1,'C003',true);
+
+
 -- PRESETS.
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
--- node_server/SQL/coupon.sql
+++ node_server/SQL/coupon.sql
@@ -2,13 +2,29 @@ DROP TABLE IF EXISTS coffee.coupon;
 CREATE TABLE coffee.coupon (
   couponID int AUTO_INCREMENT PRIMARY KEY,-- Primary key:庫碰編號
   couponName varchar(20) collate utf8_unicode_ci NOT NULL,-- 庫碰名稱
-  productID int(10) ZEROFILL NOT NULL,-- 對應商品
-  customerID varchar(5) NOT NULL,-- 持有人
-  minus int(2) ZEROFILL NOT NULL,-- 扣除額
+  minus int NOT NULL, -- 扣除額
+  limitation int, -- 折扣門檻
   cpendDate date default NULL, -- 過期時間
-  CONSTRAINT pair UNIQUE (productID,customerID)
+  used boolean
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
 
-CREATE INDEX productID ON coffee.coupon (productID);
+INSERT INTO coffee.coupon (couponName,minus,limitation,cpendDate)
+ VALUES ('20元折抵',20,200,'2020-06-30'),
+		('30元折抵',30,300,'2020-06-30'),
+		('40元折抵',40,400,'2020-06-30'),
+		('100元折抵',100,1000,'2020-06-30');
 
-INSERT INTO coffee.coupon (couponName,productID,customerID,minus,cpendDate) VALUES ('20元折抵',0000164413,20,'C001','2020-06-30'),('20元折抵',0000164415,20,'C001','2020-06-30'),('20元折抵',0000164513,20,'C001','2020-06-30');
+
+
+DROP TABLE IF EXISTS coffee.couponMap;
+CREATE TABLE coffee.couponMap (
+  couponMapId  int AUTO_INCREMENT PRIMARY KEY,-- Primary key:編號
+  couponID int ,-- 庫碰編號
+  customerID varchar(5) NOT NULL,-- 持有人
+  valid boolean,
+  CONSTRAINT pair UNIQUE (couponID,customerID)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+
+INSERT INTO coffee.couponMap (couponID,customerID,valid)
+ VALUES (1,'C001',true),(2,'C001',true),(1,'C002',true),(3,'C001',true),(3,'C002',true),(1,'C005',true),(1,'C003',true);
+
