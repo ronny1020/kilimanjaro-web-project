e6315cb5cb6998b98f30374b36b8d7f855e88242
Ronny Hsieh <1020ronny@gmail.com>
2020-04-29 23:16:49 +0800

    order SQL

---
+++ node_server/SQL/oreder.sql
@@ -0,0 +1,85 @@
+-- table訂單: orders
+-- 刪除已存在之重複table
+DROP TABLE IF EXISTS orders;
+CREATE TABLE orders(
+  OrderID int AUTO_INCREMENT PRIMARY KEY,-- Primary key:流水號
+  CustomerID varchar(5) NOT NULL,-- 買家
+  OrderDate date NOT NULL, -- 下單日期
+  ShippedDate date, -- 運送日期
+  ShippingMethodID int NOT NULL,  -- 參照ShippingMethod的配送方式
+  Freight int NOT NULL,     -- 運費
+  PaymentMethodID int NOT NULL,  -- 參照PaymentMethod表的付款方式
+  InvoiceMethodID int NOT NULL, -- 參照InvoiceMethod表的發票方式
+  RecipientName varchar(20) collate utf8_unicode_ci NOT NULL, -- 收件人姓名
+  RecipientAddress varchar(60) NOT NULL, -- 收件人地址
+  RecipientMobile varchar(24) NOT NULL, -- 收件人手機
+  couponMapId int, -- 使用的優惠券
+  rewardsPoints int -- 使用的紅利點數
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 預設儲存引擎: InnoDB(after php5.5)
+
+-- add dummy datas:
+INSERT INTO `orders` (CustomerID, OrderDate, ShippedDate, ShippingMethodID, Freight, PaymentMethodID, InvoiceMethodID,
+RecipientName, RecipientAddress, RecipientMobile, couponMapId, rewardsPoints) VALUES 
+('C001', '2020-04-29', '2020-04-30',  1, 0, 1, 1, 'Tom', 'Taipei','0937200000',null ,0 ),
+('C002', '2020-04-30', '2020-05-01',  1, 0, 2, 1, 'Jhon', 'Taipei','0937200000',null ,0 ),
+('C002', '2020-04-29', '2020-05-01',  1, 0, 1, 2, 'Jhon', 'Taipei','0937200000',null ,0 );
+
+
+
+
+DROP TABLE IF EXISTS ShippingMethod;
+CREATE TABLE ShippingMethod (
+  ShippingMethodID int  AUTO_INCREMENT  PRIMARY KEY, 
+  ShippingMethodName varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO coffee.ShippingMethod (ShippingMethodName) VALUES 
+('7-11取貨'),
+('宅配到府');
+
+DROP TABLE IF EXISTS PaymentMethod;
+CREATE TABLE PaymentMethod (
+  PaymentMethodID int  AUTO_INCREMENT  PRIMARY KEY, 
+  PaymentMethodName varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO coffee.PaymentMethod (PaymentMethodName) VALUES 
+('7-11取貨付款'),
+('ATM付款'),
+('線上刷卡');
+
+DROP TABLE IF EXISTS InvoiceMethod;
+CREATE TABLE InvoiceMethod (
+  InvoiceMethodID int  AUTO_INCREMENT  PRIMARY KEY, 
+  InvoiceMethodName varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO coffee.InvoiceMethod (InvoiceMethodName) VALUES 
+('捐贈發票'),
+('二聯電子發票'),
+('三聯電子發票');
+
+
+-- table訂單詳細: orders_detail
+-- 刪除已存在之重複table
+DROP TABLE IF EXISTS orders_detail;
+CREATE TABLE orders_detail (
+  OrderID varchar(5) NOT NULL default '',-- Primary key:流水號(訂單)
+  productID int(10) ZEROFILL,
+  Quantity smallint(6) NOT NULL ,-- 數量
+  OrderPrice int NOT NULL -- 折扣
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 預設儲存引擎: InnoDB(after php5.5)
+
+-- add dummy datas:
+INSERT INTO orders_detail (orderID, productID, Quantity, OrderPrice) VALUES
+(1, 0000164493, 49, 0.03),
+(1, 0000165302, 15, 0.8),
+(2, 0000164428, 30, 0.69),
+(1, 0000164449, 36, 0.21),
+(1, 0000165364, 42, 0.17),
+(2, 0000164817, 35, 0.97),
+(1, 0000164448, 25, 0.42),
+(3, 0000164564, 3, 0.95),
+(2, 0000165412, 30, 0.56);
