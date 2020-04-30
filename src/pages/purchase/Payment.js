ef417376cd54f42b3f06560ee177fb4ad9f7be43
Ronny Hsieh <1020ronny@gmail.com>
2020-04-30 11:42:49 +0800

    purchase

--- src/pages/purchase/Payment.js
+++ src/pages/purchase/Payment.js
@@ -2,25 +2,171 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PurchaseStepper from '../../components/purchase/PurchaseStepper'
 import { useHistory } from 'react-router-dom'
+import CardSecondary from '../../components/CardSecondary'
+import Radio from '@material-ui/core/Radio'
 
 function Payment(props) {
   let history = useHistory()
   const { Cart } = props
+
+  const [paymentMethodValue, setPaymentMethodValue] = React.useState('a')
+  const paymentMethodVChange = (event) => {
+    setPaymentMethodValue(event.target.value)
+  }
+
+  const [invoiceValue, setInvoiceValue] = React.useState('a')
+  const invoiceChange = (event) => {
+    setInvoiceValue(event.target.value)
+  }
 
   console.log(Cart)
 
   return (
     <>
       <PurchaseStepper activeStep="3" />
-      <div className="container">
+
+      <div className="container p-0">
+        <div className=" bg-primary titleLabel mt-5">
+          <h4 className="text-secondary">折價券</h4>
+        </div>
+      </div>
+      <CardSecondary>
+        <label for="coupon" class="row m-3">
+          <div className="col-md-3 d-flex align-items-center">選擇折價券</div>
+          <div className="col-md-9 d-flex align-items-center">
+            <select class="form-control" id="coupon">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </div>
+        </label>
+      </CardSecondary>
+
+      <div className="container p-0">
+        <div className=" bg-primary titleLabel mt-5">
+          <h4 className="text-secondary">點數折抵</h4>
+        </div>
+      </div>
+      <CardSecondary>
+        <label for="rewordPoint" class="row m-3">
+          <div className="col-md-3 d-flex align-items-center">點數折抵數量</div>
+          <div className="col-md-9 d-flex align-items-center">
+            <input type="number" class="form-control" id="rewordPoint" />
+          </div>
+        </label>
+      </CardSecondary>
+
+      <div className="container p-0">
+        <div className=" bg-primary titleLabel mt-5">
+          <h4 className="text-secondary">付款方式</h4>
+        </div>
+      </div>
+      <CardSecondary>
+        <label for="by711" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={paymentMethodValue === '1'}
+              onChange={paymentMethodVChange}
+              value="1"
+              color="primary"
+              name="paymentMethod"
+              id="by711"
+              inputProps={{ 'aria-label': '1' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">7-11取貨付款</div>
+        </label>
+        <label for="byATM" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={paymentMethodValue === '2'}
+              onChange={paymentMethodVChange}
+              value="2"
+              color="primary"
+              name="paymentMethod"
+              id="byATM"
+              inputProps={{ 'aria-label': '2' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">ATM付款</div>
+        </label>
+        <label for="byOnline" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={paymentMethodValue === '3'}
+              onChange={paymentMethodVChange}
+              value="3"
+              color="primary"
+              name="paymentMethod"
+              id="byOnline"
+              inputProps={{ 'aria-label': '3' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">線上刷卡</div>
+        </label>
+      </CardSecondary>
+
+      <div className="container p-0">
+        <div className=" bg-primary titleLabel mt-5">
+          <h4 className="text-secondary">發票</h4>
+        </div>
+      </div>
+      <CardSecondary>
+        <label for="donate" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={invoiceValue === '1'}
+              onChange={invoiceChange}
+              value="1"
+              color="primary"
+              name="invoice"
+              id="donate"
+              inputProps={{ 'aria-label': '1' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">捐贈發票</div>
+        </label>
+        <label for="duplicateUniform" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={invoiceValue === '2'}
+              onChange={invoiceChange}
+              value="2"
+              color="primary"
+              name="invoice"
+              id="duplicateUniform"
+              inputProps={{ 'aria-label': '2' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">二聯電子發票</div>
+        </label>
+        <label for="triplicateUniform" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={invoiceValue === '3'}
+              onChange={invoiceChange}
+              value="2"
+              color="primary"
+              name="invoice"
+              id="triplicateUniform"
+              inputProps={{ 'aria-label': '3' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">三聯電子發票</div>
+        </label>
+      </CardSecondary>
+
+      <div className="container p-0">
         <button
-          className="btn btn-success m-1"
+          className="btn btn-success mt-5"
           onClick={(e) => {
             e.preventDefault()
             history.push('/purchaseCheck')
           }}
         >
-          NEXT
+          下一步
         </button>
       </div>
     </>
--- src/pages/purchase/Shipment.js
+++ src/pages/purchase/Shipment.js
@@ -4,27 +4,127 @@ import PurchaseStepper from '../../components/purchase/PurchaseStepper'
 import { useHistory } from 'react-router-dom'
 import CardSecondary from '../../components/CardSecondary'
 
+import Radio from '@material-ui/core/Radio'
+import Checkbox from '@material-ui/core/Checkbox'
+
 function Shipment(props) {
   let history = useHistory()
   const { Cart } = props
+
+  const [shippingMethodValue, setShippingMethodValue] = React.useState('a')
+  const shippingMethodVChange = (event) => {
+    setShippingMethodValue(event.target.value)
+  }
 
   console.log(Cart)
 
   return (
     <>
       <PurchaseStepper activeStep="2" />
-      <div className="container">
-        <CardSecondary>
-          <h3>訂購人資料</h3>
-        </CardSecondary>
+
+      <div className="container p-0">
+        <div className=" bg-primary titleLabel mt-5">
+          <h4 className="text-secondary">訂購人資料</h4>
+        </div>
+      </div>
+      <CardSecondary>
+        <label for="sameAsCustomer" class="row">
+          <div className="col-md-3 d-flex align-items-center">同會員資料</div>
+          <div className="col-md-9 d-flex align-items-center">
+            <Checkbox
+              defaultChecked
+              color="primary"
+              id="sameAsCustomer"
+              inputProps={{ 'aria-label': 'secondary checkbox' }}
+            />
+          </div>
+        </label>
+        <label for="RecipientName" class="row  my-3">
+          <div className="col-md-3 d-flex align-items-center">訂購人姓名：</div>
+          <div className="col-md-9 d-flex align-items-center">
+            <input
+              type="text"
+              class="form-control"
+              id="RecipientName"
+              required
+            />
+          </div>
+        </label>
+        <label for="RecipientMobile" class="row  my-3">
+          <div className="col-md-3 d-flex align-items-center">訂購人電話：</div>
+          <div className="col-md-9 d-flex align-items-center">
+            <input
+              type="number"
+              class="form-control"
+              id="RecipientMobile"
+              required
+            />
+          </div>
+        </label>
+        <label for="RecipientAddress" class="row my-3">
+          <div className="col-md-3 d-flex align-items-center">訂購人地址：</div>
+          <div className="col-md-9 d-flex align-items-center">
+            <input
+              type="text"
+              class="form-control"
+              id="RecipientAddress"
+              required
+            />
+          </div>
+        </label>
+      </CardSecondary>
+
+      <div className="container p-0">
+        <div className=" bg-primary titleLabel mt-5">
+          <h4 className="text-secondary">配送方式</h4>
+        </div>
+      </div>
+      <CardSecondary>
+        <label for="to711" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={shippingMethodValue === '1'}
+              onChange={shippingMethodVChange}
+              value="1"
+              color="primary"
+              name="shippingMethod"
+              id="to711"
+              inputProps={{ 'aria-label': '1' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">7-11取貨</div>
+        </label>
+        <label for="toHome" class="row">
+          <div className="col-3 d-flex justify-content-center">
+            <Radio
+              checked={shippingMethodValue === '2'}
+              onChange={shippingMethodVChange}
+              value="2"
+              color="primary"
+              name="shippingMethod"
+              id="toHome"
+              inputProps={{ 'aria-label': '1' }}
+            />
+          </div>
+          <div className="col-9 d-flex align-items-center">宅配到府</div>
+        </label>
+      </CardSecondary>
+      <CardSecondary>
+        <div class="row">
+          <div className="col-3 d-flex justify-content-center">運費小計</div>
+          <div className="col-9 d-flex align-items-center">0 元</div>
+        </div>
+      </CardSecondary>
+
+      <div className="container p-0">
         <button
-          className="btn btn-success m-1"
+          className="btn btn-success mt-5"
           onClick={(e) => {
             e.preventDefault()
             history.push('/payment')
           }}
         >
-          NEXT
+          下一步
         </button>
       </div>
     </>
