ee62bc8176d613257a1594eb4b2520a91df115af
Linpoche <59193156+Linpoche@users.noreply.github.com>
2020-05-05 15:46:47 +0800

    0505

--- src/pages/OnSale.js
+++ src/pages/OnSale.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { Row } from 'react-bootstrap'
 
 import LobbyTitle from '../components/member/LobbyTitle'
