96327ae1d696708ba7bce8b0dc32a3a884644b0d
Ronny <1020ronny@gmail.com>
2020-05-12 17:43:42 +0800

    fix

--- src/components/PopularProducts.js
+++ src/components/PopularProducts.js
@@ -73,6 +73,10 @@ function PopularProducts(props) {
             <Link
               to={'../product/' + product.productID}
               className="linkNoUnderline"
+              onClick={() => {
+                document.body.scrollTop = 0 // For Safari
+                document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+              }}
             >
               <ProductListItem>
                 <div className="productListBlockView">
